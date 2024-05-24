1cb2d6bd156d5943c440039f24464662332e6803
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2024-05-24 19:17:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
+## the imports ##
 import streamlit as st
-from ollama import Ollama, OllamaException
-import httpx
+import ollama
 
-ollama = Ollama()
+## the title
+st.title("♿ ALL Wheelchair Chatbot")
 
-def generate_response():
-    try:
-        response = ollama.chat(model='phi3', stream=True, messages=st.session_state.messages)
-        for partial_resp in response:
-            token = partial_resp["message"]["content"]
-            st.session_state["full_message"] += token
-            yield token
-    except (OllamaException, httpx.HTTPStatusError) as e:
-        st.error(f"Error during Ollama request: {e}")
-    except httpx.ConnectError as e:
-        st.error(f"Connection error: {e}")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
-
-# Your Streamlit app logic
+## the first message of the AI assisstant ##
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hello Wheelchair User! How can I help you today?"}]
 
-prompt = st.text_input("Enter your message:")
-if prompt:
+### Message History ##
+for msg in st.session_state.messages:
+    if msg["role"] == "user":
+        st.chat_message(msg["role"], avatar="♿").write(msg["content"])
+    else:
+        st.chat_message(msg["role"], avatar="🤖").write(msg["content"])
+
+## Configure the model to use (in our case, the Phi-3)
+def generate_response():
+    response = ollama.chat(model='phi3', stream=True, messages=st.session_state.messages)
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        st.session_state["full_message"] += token
+        yield token
+
+if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user", avatar="🧑‍💻").write(prompt)
     st.session_state["full_message"] = ""
-    for response in generate_response():
-        st.chat_message("assistant", avatar="🤖").write(response)
+    st.chat_message("assistant", avatar="🤖").write_stream(generate_response)
     st.session_state.messages.append({"role": "assistant", "content": st.session_state["full_message"]})
